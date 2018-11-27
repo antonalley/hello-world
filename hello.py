53356afcd6660da121f2337308a13b3adcc62a31
Anton Alley <Anton.Alley@gmail.com>
2018-11-27 14:21:54 -0700

    Update hello.py
    Commented code

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 #hello.py
 
-import numpy as np
+import numpy as np # A great library
 
-x = np.random.uniform(0,1,(10,10))
+x = np.random.uniform(0,1,(10,10)) # Random 10x10 array with floats between 0 and 1
 
 print(x)
 
-print("hello world!")
+print("hello world!")# Prints Hello World\
+
+
+'''
+These are useless comments, just trying this out
+'''
+
+
+
